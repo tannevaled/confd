4804052ea1634615d3b1f3def6026116beec762f
Britt Treece <britt@spreedly.com>
2024-09-24 15:19:56 -0500

    enable initial acm test

--- test/integration/acm/test.sh
+++ test/integration/acm/test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -x
 
-export AWS_ACCESS_KEY_ID="test"
-export AWS_SECRET_ACCESS_KEY="test"
-export AWS_DEFAULT_REGION="us-east-1"
-export AWS_REGION="us-east-1"
+export AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID"
+export AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
+export AWS_DEFAULT_REGION="$AWS_DEFAULT_REGION"
+export AWS_REGION="$AWS_REGION"
 
 CERTIFICATE_ID=$(aws acm request-certificate --domain-name www.example.com \
    --validation-method DNS \
    --idempotency-token 1234 \
    --options CertificateTransparencyLoggingPreference=DISABLED \
-   --endpoint-url http://localhost:4566 --output text | cut -d '/' -f 2)
+   --endpoint-url $ACM_ENDPOINT_URL --output text | cut -d '/' -f 2)
 
 echo $CERTIFICATE_ID
